284431a7c8d25046cf5b5ec72f51f32917d704a8
Saad5400 <sdbtwa@gmail.com>
2026-04-15 01:27:00 +0300

    update cities

--- app/Enums/SaudiCity.php
+++ app/Enums/SaudiCity.php
@@ -4,35 +4,58 @@ namespace App\Enums;
 
 enum SaudiCity: string
 {
+    // تصنيف (أ)
     case Riyadh = 'الرياض';
-    case Jeddah = 'جدة';
     case Mecca = 'مكة المكرمة';
     case Medina = 'المدينة المنورة';
     case Dammam = 'الدمام';
     case Khobar = 'الخبر';
     case Dhahran = 'الظهران';
-    case Jubail = 'الجبيل';
-    case Hofuf = 'الأحساء (الهفوف)';
-    case Tabuk = 'تبوك';
-    case Abha = 'أبها';
-    case KhamisMushait = 'خميس مشيط';
-    case Taif = 'الطائف';
+    case Jeddah = 'جدة';
     case Buraydah = 'بريدة';
-    case Hail = 'حائل';
-    case Najran = 'نجران';
-    case Yanbu = 'ينبع';
-    case HafrAlBatin = 'حفر الباطن';
+    case Abha = 'أبها';
     case Jizan = 'جازان';
+    case Hail = 'حائل';
+    case Tabuk = 'تبوك';
+    case Najran = 'نجران';
     case Sakaka = 'سكاكا';
-    case Arar = 'عرعر';
-    case AlQatif = 'القطيف';
-    case AlKharj = 'الخرج';
-    case AlJouf = 'الجوف';
-    case Unayzah = 'عنيزة';
-    case Wajh = 'الوجه';
-    case Lith = 'الليث';
-    case Qunfudhah = 'القنفذة';
     case Bahah = 'الباحة';
+    case Arar = 'عرعر';
+    case Taif = 'الطائف';
+    case HafrAlBatin = 'حفر الباطن';
+    case AlAhsa = 'الأحساء';
+    case Diriyah = 'الدرعية';
+    case AlUla = 'العلا';
+
+    // تصنيف (ب)
+    case AlKharj = 'الخرج';
+    case Yanbu = 'ينبع';
+    case AlQatif = 'القطيف';
+    case Unayzah = 'عنيزة';
+    case KhamisMushait = 'خميس مشيط';
+    case AlMajmaah = 'المجمعة';
+    case Zulfi = 'الزلفي';
+    case WadiAlDawasir = 'وادي الدواسر';
+    case AlDuwadimi = 'الدوادمي';
+    case Shaqra = 'شقراء';
+    case Afif = 'عفيف';
+    case AlQuwaiiyah = 'القويعية';
+    case Rabigh = 'رابغ';
+    case Jubail = 'الجبيل';
+    case AlKhafji = 'الخفجي';
+    case Buqayq = 'بقيق';
+    case AlRass = 'الرس';
+    case AlBukairiyah = 'البكيرية';
+    case AlMuzahmiyah = 'المذنب';
+    case Bisha = 'بيشة';
+    case DhahranAlJanoub = 'ظهران الجنوب';
+    case Namas = 'النماص';
+    case MuhayilAsir = 'محايل عسير';
+    case Baljurashi = 'بلجرشي';
+    case Tayma = 'تيماء';
+    case Sabya = 'صبياء';
+    case Fifa = 'فيفا';
+    case AlQurayyat = 'القريات';
 
     /** @return array<string, string> value => label pairs for select options */
     public static function toOptions(): array
